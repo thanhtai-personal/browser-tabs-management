2dccc490b6a83d68824abb19048617bca4430cb4
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2019-10-23 12:58:07 +0700

    init system

---
+++ src/tabManager/index.js
@@ -0,0 +1,50 @@
+const TabManagement = () => {
+
+  const dataKeys = {
+    tabsManagement: 'tabs-management',
+    privateKeys: ['id']
+  }
+
+  let { tabList } = JSON.parse(window.localStorage.getItem(dataKeys.tabsManagement))
+  if (!tabList) {
+    tabList = []
+  }
+
+  const getTabList = () => {
+    return tabList
+  }
+
+  const newTab = (data) => {
+    let tabData = {
+      id: new Date()
+    }
+    if (data && typeof data === 'object') {
+      Object.keys(data).forEach((key) => {
+        if (!dataKeys.privateKeys.includes(key)) tabData[key] = data[key]
+      })
+    }
+    tabList.push(tabData)
+    window.localStorage.setItem(dataKeys.tabsManagement, JSON.stringify({ ...JSON.parse(window.localStorage.getItem(dataKeys.tabsManagement)), tabList: tabList }))
+    return tabData
+  }
+
+  const removeTab = (id) => {
+    let tabIndex = tabList.findIndex((tab) => tab.id === id)
+    if (tabIndex >= 0) tabList.splice(tabIndex, 1)
+    tabList.push(tabData)
+    window.localStorage.setItem(dataKeys.tabsManagement, JSON.stringify({ ...JSON.parse(window.localStorage.getItem(dataKeys.tabsManagement)), tabList: tabList }))
+  } 
+
+  const getTab = (id) => {
+    return tabList.find((tab) => tab.id === id)
+  }
+
+  return {
+    newTab: newTab,
+    getTab: getTab,
+    removeTab: removeTab,
+    getTabList: getTabList
+  }
+}
+
+export default TabManagement
