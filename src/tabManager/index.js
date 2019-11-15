e8cc366e9fd79f062d997a662bdefeb2ddc7af56
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2019-11-15 11:39:34 +0700

    fix crash browser issue

--- src/tabManager/index.js
+++ src/tabManager/index.js
@@ -1,6 +1,7 @@
 const TabManagement = (appKey) => {
 
   const scanTime = 3000
+  let isAutoScan = false
 
   const onlineState = {
     inactive: 'inactive',
@@ -113,35 +114,44 @@ const TabManagement = (appKey) => {
 
   const scanInactiveTab = (callBack) => {
     let tabList = getTabList()
-    let tabScan = JSON.parse(window.localStorage.getItem(dataKeys.tabScan)) || []
-    let tabId = ''
-    if (tabScan.length > 0) {
-      tabId = tabScan[tabScan.length - 1]
-    }
-    tabList.forEach(tab => {
-      setTab(tab.id, [{ key: dataKeys.isOnline, value: onlineState.inactive }])
+    let tabScan = getData(dataKeys.tabScan) || []
+    let tabId = tabScan !== [] ? tabScan[tabScan.length - 1] : ''
+    tabList = tabList.map(tab => {
+      if (tab.id !== tabId) {
+        setTab(tab.id, [{ key: dataKeys.isOnline, value: onlineState.inactive }])
+      } else {
+        setTab(tab.id, [{ key: dataKeys.isOnline, value: onlineState.active }])
+      }
+      return tab
+    })
+    tabList.forEach((tab) => {
+      setTab(tab.tabId, Object.keys(tab).map((key) => {
+        return {
+          key: key,
+          value: tab[key]
+        }
+      }))
     })
     setManagerData([{ key: dataKeys.isScan, value: true }])
-    setTimeout(() => {
+    let to = setTimeout(() => {
       tabList = getTabList()
       tabList.forEach((tab) => {
-        if (tab[dataKeys.isOnline] === onlineState.inactive
-          && tab.id !== tabId
-          ) {
+        if (tab[dataKeys.isOnline] === onlineState.inactive) {
           removeTab(tab.id)
         } else {
           setTab(tab.id, [{ key: dataKeys.isOnline, value: true }])
         }
       })
       typeof callBack === 'function' && callBack()
-      setManagerData([{ key: dataKeys.isScan, value: false }])
-      window.localStorage.setItem(dataKeys.tabScan, JSON.stringify([]))
+      setManagerData([{ key: dataKeys.isScan, value: false }
+      , { key: dataKeys.tabScan, value: [] } ])
+      clearTimeout(to)
     }, scanTime)
   }
 
   
 
-  const newTab = (data , callBack) => {
+  const newTab = (data, isAutoScan, callBack) => {
     let tabData = {
       id: `${dataKeys.tabPrefix}_${getNow().valueString}`,
       isOnline: true
@@ -152,10 +162,13 @@ const TabManagement = (appKey) => {
       })
     }
     window.localStorage.setItem(tabData.id, JSON.stringify(tabData))
-    let tabScan = JSON.parse(window.localStorage.getItem(dataKeys.tabScan)) || []
-    tabScan.push(tabData.id)
-    window.localStorage.setItem(dataKeys.tabScan, JSON.stringify(tabScan))
-    scanInactiveTab()
+    if (isAutoScan) {
+      isAutoScan = true
+      let tabScan = JSON.parse(window.localStorage.getItem(dataKeys.tabScan)) || []
+      tabScan.push(tabData.id)
+      window.localStorage.setItem(dataKeys.tabScan, JSON.stringify(tabScan))
+      scanInactiveTab()
+    }
     typeof callBack === 'function' && callBack()
     return tabData
   }
@@ -210,6 +223,19 @@ const TabManagement = (appKey) => {
         }
       }
     })
+    if (!isAutoScan) {
+      if (getData(dataKeys.isScan)) {
+        setTab(tabData.id, [{ key: dataKeys.isOnline, value: onlineState.active }])
+      } else {
+        let tabScan = getData(dataKeys.tabScan) || []
+        tabScan.push(tabData.id)
+        setManagerData([{ key: dataKeys.tabScan, value: tabScan }])
+        tabScan = getData(dataKeys.tabScan) || []
+        if (!_.isEmpty(tabScan) && tabData.id === tabScan[tabScan.length - 1]) {
+          scanInactiveTab()
+        }
+      }
+    }
   }
 
   return {
